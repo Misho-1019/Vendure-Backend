b1db5b11fa2e2769b66d5c06e6f7e89bf3506763
Misho1019 <mikailtorres99@gmail.com>
2025-08-27 17:15:01 +0300

    add shared Create/Update input interfaces

--- src/plugins/site-settings/types.ts
+++ src/plugins/site-settings/types.ts
@@ -1,7 +1,19 @@
-/**
- * @description
- * The plugin can be configured using the following options:
- */
-export interface PluginInitOptions {
-    exampleOption?: string;
+import { CustomFieldsObject } from '@vendure/common/lib/shared-types';
+import { ID } from '@vendure/core';
+
+export interface CreateSiteSettingsInput {
+  key: string;
+  title: string;
+  primaryColor: string;
+  accountHeading: string;
+  customFields?: CustomFieldsObject;
+}
+
+export interface UpdateSiteSettingsInput {
+  id: ID;
+  key?: string;
+  title?: string;
+  primaryColor?: string;
+  accountHeading?: string; // optional on update
+  customFields?: CustomFieldsObject;
 }
